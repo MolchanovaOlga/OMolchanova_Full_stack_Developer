14279b4cbdc5b9f9fe38df8ffcd357d8c26b6ed7
MolchanovaOlga <designer.molchanova@gmail.com>
2024-05-07 14:15:18 +0300

    Change import in main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import './js/portfolio.js';
 
 // --------------------- attachment of pdf-file to menu-resume ---------------------
 
-import pdfUrl from './public/files/Olga_Molchanova_Front_end_Developer.pdf';
+import pdfUrl from './public/files/Olga_Molchanova_Full_stack_Developer.pdf';
 document.getElementById('link-pdf').href = pdfUrl;
